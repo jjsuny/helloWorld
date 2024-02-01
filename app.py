19d2bb25fd0afa6c37c9e0986849d4a972e69dda
jjsun <jjsun@terpmail.umd.edu>
2024-01-31 20:51:00 -0500

    html changes commit

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World from Jeff Jiang! '
+    return 'Hello World from Jeff Jiang! HTML changes '
+
 
 if __name__ == '__main__':
     app.run()
