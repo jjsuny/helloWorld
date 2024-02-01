f4866fab680235dda16f5c8c7b7b22d6a64aa7b1
jjsun <jjsun@terpmail.umd.edu>
2024-01-31 20:51:15 -0500

    commit with html website changes

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World from Jeff Jiang! HTML changes '
 
+@app.route('/hello')
+def hello():
+    return render_template('hello.html')
 
 if __name__ == '__main__':
     app.run()
